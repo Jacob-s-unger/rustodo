a74bd109151f7d766a049a09b63a7b80a1d346d5
jacob-s-unger <jacob.s.unger@gmail.com>
2023-06-13 09:26:02 -0600

    initial

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,7 @@ struct Cli { //This is our Struct
 
 fn main() {
     let args = Cli::parse(); //the passed args are run throught the Cli Parse functionality, this is the bread and butter of breaking up arguments
-    // let todo = std::io::read_to_string(&args.todo).expect("please repeat your task"); 
-    // let urgent = std::io::read_to_string(&args.urgency).expect("Whoops, missed an input");
 
-    print!("Your task for today is {}, and its urgency is {} \n", args.todo, args.urgency)
+    print!("Your task for today is {}, and its urgency is {} \n", args.todo, args.urgency) //you then call these arguments by args.name of the attribute
 }
 
-
-//So when we run the command 'cargo run -- main src/main.rs' we are looking for the pattern = "main" in the file = 'src/main.rs'
-// once the pattern is found in that file, we print that entire line, so the output that we get is 'fn main() {'
-// NOTE: it will print all instances of that found pattern in the file, meaning it is iterating over the entire file...!x
-// running different input arguments will cement this idea...
-//Try things like 'cargo run -- pattern src/main.rs' and ' cargo run -- Cli src/main.rs' or even running it on different files! Just make sure to specify path right 
